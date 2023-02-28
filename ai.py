4d317cb1d03f76b0da33ae2eca09fda8cde973d0
Kappo99 <andrea30.4.99@gmail.com>
2023-02-28 17:43:09 +0100

    BFS + A* v2

--- ai.py
+++ ai.py
@@ -1,8 +1,20 @@
-from queue import Queue
 import algorithm
-import heapq
+import pprint
 import json
 import time
+
+def path_to_list(path):
+    list = []
+    for key, value in path:
+        found = False
+        for group in list:
+            if group[0] == key:
+                group[1].append(value)
+                found = True
+                break
+        if not found:
+            list.append((key, [value]))
+    return sorted(list, key=lambda x: x[0])
 
 def test_from_file(test_number):
     with open("config/"+str(test_number)+".json", "r") as f:
@@ -18,31 +30,31 @@ def test_from_file(test_number):
     print("K: ", k)
     print("N: ", N)
 
-    # bfs_start_time = time.time() # memorizza il tempo di inizio
-    # bfs_path = algorithm.bfs(start, goal, k, N) # esegue l'algoritmo
-    # bfs_end_time = time.time() # memorizza il tempo di fine
-    # bfs_duration = bfs_end_time - bfs_start_time # calcola la durata in secondi
+    bfs_start_time = time.time() # memorizza il tempo di inizio
+    bfs_path = algorithm.bfs(start, goal, k, N) # esegue l'algoritmo
+    bfs_end_time = time.time() # memorizza il tempo di fine
+    bfs_duration = bfs_end_time - bfs_start_time # calcola la durata in secondi
 
-    dfs_start_time = time.time() # memorizza il tempo di inizio
-    dfs_path = algorithm.dfs(start, goal, k, N, set()) # esegue l'algoritmo
-    dfs_end_time = time.time() # memorizza il tempo di fine
-    dfs_duration = dfs_end_time - dfs_start_time # calcola la durata in secondi
+    # dfs_start_time = time.time() # memorizza il tempo di inizio
+    # dfs_path = algorithm.dfs(start, goal, k, N, set()) # esegue l'algoritmo
+    # dfs_end_time = time.time() # memorizza il tempo di fine
+    # dfs_duration = dfs_end_time - dfs_start_time # calcola la durata in secondi
 
-    # a_star_start_time = time.time() # memorizza il tempo di inizio
-    # a_star_path = algorithm.a_star(start, goal, k, N) # esegue l'algoritmo
-    # a_star_end_time = time.time() # memorizza il tempo di fine
-    # a_star_duration = a_star_end_time - a_star_start_time # calcola la durata in secondi
+    a_star_start_time = time.time() # memorizza il tempo di inizio
+    a_star_path = algorithm.a_star(start, goal, k, N) # esegue l'algoritmo
+    a_star_end_time = time.time() # memorizza il tempo di fine
+    a_star_duration = a_star_end_time - a_star_start_time # calcola la durata in secondi
     
     with open("output/"+str(test_number)+".txt", 'w') as f:
-        # f.write("BFS\n" + "Durata: " + str(bfs_duration) + " secondi\n" + str(bfs_path) + "\n\n")
-        f.write("DFS\n" + "Durata: " + str(dfs_duration) + " secondi\n" + str(dfs_path) + "\n\n")
-        # f.write("A*\n" + "Durata: " + str(a_star_duration) + " secondi\n" + str(a_star_path) + "\n\n")
+        f.write("BFS\n" + "Durata: " + str(bfs_duration) + " secondi\n")
+        pprint.pprint(path_to_list(bfs_path), width=15, stream=f)
+        f.write("\n\n")
+        # f.write("DFS\n" + "Durata: " + str(dfs_duration) + " secondi\n")
+        # pprint.pprint(path_to_list(dfs_path), width=15, stream=f)
+        # f.write("\n\n")
+        f.write("A*\n" + "Durata: " + str(a_star_duration) + " secondi\n")
+        pprint.pprint(path_to_list(a_star_path), width=15, stream=f)
+        f.write("\n\n")
 
-# test_number = int(input("Test number: "))
-# test_from_file(test_number)
-
-start = ((0, 0), (1, 0), (2, 0)) # posizioni iniziali dei cavalieri
-goal = ((2, 2), (3, 2), (4, 2)) # posizioni obiettivo dei cavalieri
-N = 5 # dimensione della scacchiera
-path = algorithm.dfs(start, goal, 3, N, set()) # calcola il costo minimo della soluzione
-print(path) # stampa il costo minimo della soluzione
+test_number = int(input("Test number: "))
+test_from_file(test_number)
--- dfs.py
+++ dfs.py
@@ -1,5 +1,3 @@
-from queue import Queue
-
 def generate_moves(pos, N):
     row, col = pos # estrae la riga e la colonna della posizione
     moves = set() # insieme dei possibili movimenti
@@ -9,36 +7,45 @@ def generate_moves(pos, N):
             moves.add((r, c)) # aggiunge la nuova posizione all'insieme dei movimenti
     return moves # restituisce l'insieme dei movimenti
 
-def dfs(num_knights, positions, targets, N, visited):
-    if all(pos == target for pos, target in zip(positions, targets)):
-        # se tutte le posizioni obiettivo sono state raggiunte, restituisci le mosse effettuate
-        return [positions]
-    else:
-        # altrimenti, esplora tutte le possibili mosse dei cavalieri
-        min_moves = None
-        for i in range(num_knights):
-            for move in generate_moves(positions[i], N):
-                if move not in visited:
-                    visited.add(move)
-                    new_positions = positions[:i] + (move,) + positions[i+1:]
-                    # calcola ricorsivamente le mosse effettuate per raggiungere la soluzione ottimale
-                    moves = dfs(num_knights, new_positions, targets, N, visited)
-                    visited.remove(move)
-                    if moves is not None:
-                        # se la soluzione è valida, aggiungi la mossa corrente alla lista delle mosse effettuate
-                        moves.append(new_positions)
-                        if min_moves is None or len(moves) < len(min_moves):
-                            # se questa è la soluzione più breve trovata finora, aggiorna la lista delle mosse effettuate
-                            min_moves = moves
-        return min_moves
 
-positions = ((0, 0), (1, 0)) # posizioni iniziali dei cavalieri
-targets = ((2, 2), (2, 1)) # posizioni obiettivo dei cavalieri
-N = 3 # dimensione della scacchiera
-# positions = ((0, 0), (1, 0), (2, 0)) # posizioni iniziali dei cavalieri
-# targets = ((2, 2), (3, 2), (4, 2)) # posizioni obiettivo dei cavalieri
+def dfs(k, positions, targets, N, visited):
+    if k == len(positions):
+        return []
+    moves = []
+    for i, pos in enumerate(positions):
+        if i not in visited:
+            visited.add(i)
+            for move in generate_moves(pos, N):
+                if move == targets[k]:
+                    new_positions = positions[:i] + (move,) + positions[i+1+len(visited):]
+                    res = dfs(k+1, new_positions, targets, N, visited)
+                    if res is not None:
+                        moves.append((i, move))
+                        moves.extend(res)
+            visited.remove(i)
+    return moves
+
+
+positions = ((0, 0), (1, 1))
+targets = ((4, 2), (3, 2))
+N = 5
+visited = set()
+moves = dfs(0, positions, targets, N, visited)
+print(moves)
+
+# positions = ((0, 0), (1, 0)) # posizioni iniziali dei cavalieri
+# targets = ((2, 2), (2, 1)) # posizioni obiettivo dei cavalieri
+# N = 3 # dimensione della scacchiera
+# # positions = ((0, 0), (1, 0), (2, 0)) # posizioni iniziali dei cavalieri
+# # targets = ((2, 2), (3, 2), (4, 2)) # posizioni obiettivo dei cavalieri
+# # N = 5 # dimensione della scacchiera
+# visited = set() # insieme delle posizioni visitate
+# moves = dfs(2, positions, targets, N, visited) # calcola le mosse effettuate per raggiungere la soluzione ottimale
+# print(moves) # stampa le mosse
+
+# start = ((0, 0), (1, 0), (2, 0)) # posizioni iniziali dei cavalieri
+# goal = ((2, 2), (3, 2), (4, 2)) # posizioni obiettivo dei cavalieri
 # N = 5 # dimensione della scacchiera
-visited = set() # insieme delle posizioni visitate
-moves = dfs(2, positions, targets, N, visited) # calcola le mosse effettuate per raggiungere la soluzione ottimale
-print(moves) # stampa le mosse
+# path = algorithm.dfs(start, goal, 3, N, set()) # calcola il costo minimo della soluzione
+# print(path) # stampa il costo minimo della soluzione
 
